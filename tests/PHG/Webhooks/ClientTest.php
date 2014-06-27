82d3edffda83437d8a5018d58eda273f293a4ea5
Scott Robertson <scottymeuk@gmail.com>
2014-06-27 13:42:19 +0100

    Test ->push method

--- tests/PHG/Webhooks/ClientTest.php
+++ tests/PHG/Webhooks/ClientTest.php
@@ -8,4 +8,19 @@ class ClientTest extends \PHG\Webhooks\TestCase
 	{
 		new \PHG\Webhooks\Client('http://example.com');
 	}
+
+	public function testPush()
+	{
+		$consumer = 'testing';
+		$action = 'test';
+		$data = array(1,2,3,4);
+
+		$client = \Mockery::mock('\PHG\Webhooks\Client[post]', array('http://example.com'))
+			->shouldReceive('post')
+			->once()
+			->with($consumer . '-' . $action, $data)
+			->mock();
+
+		$client->push($consumer, $action, $data);
+	}
 }
